f4db8f173fb611e07643a2fa0cd7f332a36dbeaa
Alexey Smagin <asblackpn@gmail.com>
2016-04-14 19:32:37 +0300

    menu added

--- src/ru/miet/stepanov/Main.java
+++ src/ru/miet/stepanov/Main.java
@@ -1,9 +1,30 @@
 package ru.miet.stepanov;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println();
+        while(true) {
+            System.out.println("Выберите фигуру: \n1 - Прямоугольник\n2 - Треугольник\n3 - Параллелепипед\n4-  Призма\n5- Выйти из программы");
+            Scanner in = new Scanner(System.in);
+            int a = in.nextInt();
+            switch (a) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Введите другое значение");
+            }
+        }
 
     }
 }
