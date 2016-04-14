65f72e5df94f31b0d81a4a664ce6664483016ceb
greyfox2208 <valeriyaoleneva@yandex.ru>
2016-04-15 02:02:05 +0300

    Периметр и площадь треугольника. Нет проверки знчений

--- src/ru/miet/stepanov/Triangle.java
+++ src/ru/miet/stepanov/Triangle.java
@@ -1,7 +1,28 @@
 package ru.miet.stepanov;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.lang.Math;
+
+
 public class Triangle {
     public static void go () {
 
+    }
+
+    public static void go() {
+        System.out.println("Введите стороны a, b,c");
+        Scanner in = new Scanner(System.in);
+        double a=0, b=0,c=0;
+                in = new Scanner(System.in);
+                a = in.nextDouble();
+                b = in.nextDouble();
+                c = in.nextDouble();
+
+
+                System.out.println("Неправильно введены аргументы! Попробуйте ещё раз.\n");
+        }
+        double p=(a+b+c)/2;
+        System.out.println("Площадь: " + Math.sqrt(p*(p-a)*(p-b)*(p-c)) + ", периметр: " + 2*p + "\n");
     }
 }
