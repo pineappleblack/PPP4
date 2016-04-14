8a64d88971e952de058df645607eb9c3486de6c6
Alexey Smagin <asblackpn@gmail.com>
2016-04-14 20:47:44 +0300

    Firt version of hanling InputMismatch

--- src/ru/miet/stepanov/Rectangle.java
+++ src/ru/miet/stepanov/Rectangle.java
@@ -1,13 +1,20 @@
 package ru.miet.stepanov;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Rectangle {
     public static void go() {
         System.out.println("Введите стороны a, b");
         Scanner in = new Scanner(System.in);
-        double a = in.nextDouble();
-        double b = in.nextDouble();
+        double a=0, b=0;
+        try {
+            a = in.nextDouble();
+            b = in.nextDouble();
+        }
+        catch(InputMismatchException ex) {
+            ex.printStackTrace();
+        }
         System.out.println("Площадь: " + a*b + ", периметр: " + (a+b)*2 + "\n");
     }
 }
