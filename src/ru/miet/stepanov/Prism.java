97b7aec44263df5a4e5f2ecb34af604550bf8308
Arnik9 <impwithafurrytail@gmail.com>
2016-04-15 02:13:04 +0300

    Update Prism.java
    added formuls

--- src/ru/miet/stepanov/Prism.java
+++ src/ru/miet/stepanov/Prism.java
@@ -1,7 +1,27 @@
 package ru.miet.stepanov;
 
-public class Prism {
-    public static void go() {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Prizma {
+    public static void go() {
+        System.out.println("Введите сторону и высоту a, b");
+        Scanner in = new Scanner(System.in);
+        double a=0, b=0;
+        boolean flag=true;
+        while (flag)
+        {
+            try {
+                in = new Scanner(System.in);
+                a = in.nextDouble();
+                b = in.nextDouble();
+                flag = false;
+            }
+            catch(InputMismatchException ex) {
+                System.out.println("Неправильно введены аргументы! Попробуйте ещё раз.\n");
+            }
+        }
+        System.out.println("Площадь: " + (a*3*b) + ", периметр: " + (a*6+b*3) + "\n");
     }
 }
+
