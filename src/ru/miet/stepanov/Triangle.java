38b64c52dc61116c414f101c3654f88c7045ab00
greyfox2208 <valeriyaoleneva@yandex.ru>
2016-04-15 02:03:21 +0300

    Добавлена проверка значений

--- src/ru/miet/stepanov/Triangle.java
+++ src/ru/miet/stepanov/Triangle.java
@@ -4,7 +4,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.lang.Math;
 
-
 public class Triangle {
     public static void go () {
 
@@ -14,13 +13,19 @@ public class Triangle {
         System.out.println("Введите стороны a, b,c");
         Scanner in = new Scanner(System.in);
         double a=0, b=0,c=0;
+        boolean flag=true;
+        while (flag)
+        {
+            try {
                 in = new Scanner(System.in);
                 a = in.nextDouble();
                 b = in.nextDouble();
                 c = in.nextDouble();
-
-
+                flag = false;
+            }
+            catch(InputMismatchException ex) {
                 System.out.println("Неправильно введены аргументы! Попробуйте ещё раз.\n");
+            }
         }
         double p=(a+b+c)/2;
         System.out.println("Площадь: " + Math.sqrt(p*(p-a)*(p-b)*(p-c)) + ", периметр: " + 2*p + "\n");
